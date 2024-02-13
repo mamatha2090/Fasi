8b19678e4c8f1ee21777b96ff03f064e2a83d401
Fasi Ahmed Khan J R <148610998+Fasiahmedkhan@users.noreply.github.com>
2024-02-13 14:00:04 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,13 @@
+
+n = int(input("Enter a non-negative integer: "))
+
+if n < 0:
+    print("Please enter a non-negative integer.")
+else:
+  
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+    
+  
+    print("Factorial of",  ":", factorial)
